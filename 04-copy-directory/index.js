c09efe563c0d838dcd78f807b19f6b90deb42d1f
Dmitriy Bartashevich <3088742@mail.ru>
2021-11-09 00:49:30 +0300

    feat: add copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const path = require('path');
+const fs = require('fs');
 
+const filesName = path.join(__dirname, 'files');
+const filesCopyName = path.join(__dirname, 'files-copy');
+
+function copyDir() {
+	fs.mkdir(filesCopyName, { recursive: true }, err => {
+
+		fs.readdir(filesName, (err, files) => {
+
+			for (let file of files) {
+				let original = path.join(filesName, file)
+				let copy = path.join(filesCopyName, file)
+				fs.copyFile(original, copy, err => {
+					if (err) throw err
+				})
+			}
+		});
+	});
+}
+copyDir()
